13963900074da0b42d49408af9b609a5dd841329
James Le <le_j6@denison.edu>
2017-10-27 11:24:48 +0700

    Authentication: Add User Model

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+var passport = require("passport");
+var LocalStrategy = require("passport-local");
 var Coffeeshop = require("./models/coffeeshop");
 var Comment = require("./models/comment");
+var User = require("./models/user");
 var seedDB = require("./seeds");
 
 mongoose.connect("mongodb://localhost/yelp_coffee_shop");
