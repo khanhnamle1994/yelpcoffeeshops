24d1db41b1e5afa5fba86140fc5fb522a4bc9de5
James Le <le_j6@denison.edu>
2017-10-27 14:43:14 +0700

    User Associations: Comment

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ mongoose.connect("mongodb://localhost/yelp_coffee_shop");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-seedDB();
+// seedDB(); // seed the database
 
 // Passport Configuration
 app.use(require("express-session")({
