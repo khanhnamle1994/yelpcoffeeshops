2911e1862526b7a12f738c8835761e1070bd89d5
James Le <le_j6@denison.edu>
2017-10-29 20:35:38 +0700

    Flash Messages: Adding Bootstrap

--- routes/index.js
+++ routes/index.js
@@ -29,7 +29,7 @@ router.post("/register", function(req, res){
 
 // show login form
 router.get("/login", function(req, res){
-  res.render("login", {message: req.flash("error")});
+  res.render("login");
 });
 
 // handle login logic
@@ -43,6 +43,7 @@ router.post("/login", passport.authenticate("local",
 // logout route
 router.get("/logout", function(req, res){
   req.logout();
+  req.flash("success", "Logged you out!");
   res.redirect("/coffeeshops");
 });
 
