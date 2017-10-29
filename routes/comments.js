2bc58e670d2d5b7a7ef1d750d6a1cb16e9631944
James Le <le_j6@denison.edu>
2017-10-29 07:55:11 +0700

    Comment Destroy

--- routes/comments.js
+++ routes/comments.js
@@ -66,6 +66,17 @@ router.put("/:comment_id", function(req, res){
   });
 });
 
+// Comments Destroy
+router.delete("/:comment_id", function(req, res){
+  Comment.findByIdAndRemove(req.params.comment_id, function(err){
+    if(err){
+      res.redirect("back");
+    } else {
+      res.redirect("/coffeeshops/" + req.params.id);
+    }
+  });
+});
+
 // Middleware
 function isLoggedIn(req, res, next){
   if(req.isAuthenticated()){
