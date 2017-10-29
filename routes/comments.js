2f793bed985537f40f0f07a70b0f082170545a9c
James Le <le_j6@denison.edu>
2017-10-29 08:02:42 +0700

    Comment Authorization

--- routes/comments.js
+++ routes/comments.js
@@ -45,7 +45,7 @@ router.post("/", isLoggedIn, function(req, res){
 });
 
 // Comments Edit
-router.get("/:comment_id/edit", function(req, res){
+router.get("/:comment_id/edit", checkCommentOwnership, function(req, res){
   Comment.findById(req.params.comment_id, function(err, foundComment){
     if(err){
       res.redirect("back");
@@ -56,7 +56,7 @@ router.get("/:comment_id/edit", function(req, res){
 });
 
 // Comments Update
-router.put("/:comment_id", function(req, res){
+router.put("/:comment_id", checkCommentOwnership, function(req, res){
   Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
     if(err){
       res.redirect("back");
@@ -67,7 +67,7 @@ router.put("/:comment_id", function(req, res){
 });
 
 // Comments Destroy
-router.delete("/:comment_id", function(req, res){
+router.delete("/:comment_id", checkCommentOwnership, function(req, res){
   Comment.findByIdAndRemove(req.params.comment_id, function(err){
     if(err){
       res.redirect("back");
@@ -83,6 +83,24 @@ function isLoggedIn(req, res, next){
     return next();
   }
   res.redirect("/login");
+};
+
+function checkCommentOwnership(req, res, next) {
+  if(req.isAuthenticated()){
+    Comment.findById(req.params.comment_id, function(err, foundComment){
+      if(err){
+        res.redirect("back");
+      } else {
+        if(foundComment.author.id.equals(req.user._id)) {
+          next();
+        } else {
+          res.redirect("back");
+        }
+      }
+    });
+  } else {
+    res.redirect("back");
+  }
 };
 
 module.exports = router;
