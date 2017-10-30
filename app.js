0920c3d191eb0ca4b64a78baea1a945c272ff0c5
James Le <le_j6@denison.edu>
2017-10-30 10:48:29 +0700

    Environment Variables

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ var commentRoutes = require("./routes/comments");
 var coffeeshopRoutes = require("./routes/coffeeshops");
 var indexRoutes = require("./routes/index");
 
-// mongoose.connect("mongodb://localhost/yelp_coffee_shop");
-mongoose.connect("mongodb://khanhnamle:2017@ds241025.mlab.com:41025/yelpcoffeeshop");
+var url = process.env.DATABASEURL || "mongodb://localhost/yelp_coffee_shop";
+mongoose.connect(url);
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
